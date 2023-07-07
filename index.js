78e9cada4e9e8476e244f1030f0ebfcd6214a6a7
Jenipher Oduor <jenipheroduor@gmail.com>
2023-07-07 16:50:09 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location)
+{
+    if (location === 43)
+    {
+        return 1;
+    }
+    else if (location === 50)
+    {
+        return 8;
+    }
+    else if (location < 42)
+    {
+        return 8;
+    }
+
+}
+
+function distanceFromHqInFeet(location)
+{
+    return distanceFromHqInBlocks(location) * 264;
+
+}
+
+function distanceTravelledInFeet(start, destination)
+{
+    return (Math.abs(destination - start)) * 264;
+}
+
+function calculatesFarePrice(start, destination)
+{
+    const distance =  distanceTravelledInFeet(start,destination)
+    {
+        if (distance <= 400)
+        {
+            return 0
+        }
+        else if (distance > 400 && distance <= 2000)
+        {
+             return (distance - 400) * 0.02
+        }
+        else if (distance > 2000 && distance < 2500)
+        {
+            return 25 
+        }
+        else if (distance > 2500)
+        {
+            return "cannot travel that far"
+        }
+    }
+}
